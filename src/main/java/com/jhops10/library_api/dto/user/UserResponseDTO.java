636667c3070c822ca74ee27efb1663a50a1a21e6
João Paulo <joaowais@gmail.com>
2025-03-14 22:58:34 -0300

    feat: Adiciona UserRequestDTO e UserResponseDTO

---
+++ src/main/java/com/jhops10/library_api/dto/user/UserResponseDTO.java
@@ -0,0 +1,76 @@
+package com.jhops10.library_api.dto.user;
+
+import com.jhops10.library_api.entities.UserRole;
+
+import java.time.LocalDateTime;
+
+public class UserResponseDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private UserRole role;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public UserResponseDTO() {
+    }
+
+    public UserResponseDTO(Long id, String name, String email, String password, UserRole role, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
